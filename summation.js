67fdd281eafa5c0fae518f966fa4a31286a95f46
samekh resh <samekh.resh@gmail.com>
2021-09-21 10:21:46 -0400

    summation function

---
+++ summation.js
@@ -0,0 +1,12 @@
+let summation = function (num) {
+    let resultingNumber = 0
+    for (let i = 1; i <= num; i++){
+       resultingNumber += i;
+    }
+    return resultingNumber
+  }
+
+  //purpose of this was to create a function that gave you a sequential sum of numbers, based on the number given
+  //so if someone was given the number they would want the summation of 1 + 2 + 3 + 4 + 5 
+  
+  
