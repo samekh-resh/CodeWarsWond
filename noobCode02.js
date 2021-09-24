07010bdba59ce911e65563c69efc411f957c72e5
samekh resh <samekh.resh@gmail.com>
2021-09-24 10:05:22 -0400

    codes for yesterday and today

---
+++ noobCode02.js
@@ -0,0 +1,10 @@
+//Write a function greaterThanLessThan that takes three arguments, a, b, and c, and returns a boolean (true or false), such that:
+
+
+function greaterThanLessThan (a,b,c) {
+    if (a < b < c){
+      return true
+    }else {
+      return false
+    }
+   }
