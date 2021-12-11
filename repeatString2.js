cd98e0d9ec4f0777b2a86cc3bc0fae15388cb53f
samekh resh <samekh.resh@gmail.com>
2021-12-11 08:40:04 -0500

    codeware for the day

---
+++ repeatString2.js
@@ -0,0 +1,14 @@
+function repeatStr(n, s) {
+
+    //   return s.repeat(n)
+
+    let repeatedStr = ''
+    for (let i = 0; i < n; i++) {
+        repeatedStr += s
+    }
+    return repeatedStr;
+}
+
+    //taking in two parameters, first is the number of times it's repeating
+    //second param is the thing that is actually being repeated.
+    //many ways to do this, I think one way would be a simple loop
