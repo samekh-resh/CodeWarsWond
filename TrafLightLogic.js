776698b1d3055deae32e8300ffcce581b609af32
samekh resh <samekh.resh@gmail.com>
2021-12-15 10:47:16 -0500

    cw

---
+++ TrafLightLogic.js
@@ -0,0 +1,14 @@
+
+   
+function updateLight(current) {
+    if(current === 'green'){
+      return 'yellow'
+      }
+    
+    if(current === 'yellow'){
+      return 'red' 
+    }
+    if(current === 'red'){
+      return 'green'
+    }
+  }
