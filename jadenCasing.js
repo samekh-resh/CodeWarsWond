8f952957e43fae97f826a3c4827aa5d059f3ebad
samekh resh <samekh.resh@gmail.com>
2022-01-20 21:46:24 -0500

    codewar

---
+++ jadenCasing.js
@@ -0,0 +1,8 @@
+String.prototype.toJadenCase = function () {
+    //...
+    return this.split(" ")
+      .map(function (word) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      })
+      .join(" ");
+  };
