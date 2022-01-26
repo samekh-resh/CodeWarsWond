fa07008a768f88c40a43f9b1a8cbcb6c7eef0a26
samekh resh <samekh.resh@gmail.com>
2022-01-26 17:08:33 -0500

    codewars

---
+++ arrayPlusArrayAgain.js
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/5a2be17aee1aaefe2a000151
+
+function arrayPlusArray(arr1, arr2) {
+    return arr1.reduce((a,b) => a + b) +  arr2.reduce((a,b) => a + b)
+  }
---
+++ flipGravity.js
@@ -0,0 +1,13 @@
+const flip=(d, a)=>{
+    //TODO
+    if(d === 'R'){
+      return a.sort((a, b) => a - b)
+    }else{
+      return a.sort((a, b) => b - a)
+    }
+    
+    return 
+  }
+  
+  
+  //we are sorting the arrays either way, however l is going to have them reversed whereas r will just give them 
